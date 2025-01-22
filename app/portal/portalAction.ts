d72a43484dfd0e9cb0f17e3c57293512658c2b79
ayushsinha12 <ayushsinha2445@gmail.com>
2025-01-22 02:06:38 -0500

    Portal Updates

--- app/portal/portalAction.ts
+++ app/portal/portalAction.ts
@@ -7,7 +7,7 @@ export async function createPortalSession(customerId: string) {
     console.log('createPortalSession called with:', customerId);
     const portalSession = await stripe.billingPortal.sessions.create({
         customer: customerId,
-        return_url: `https://stripe-photography-subscription-gid13y1an.vercel.app`
+        return_url: `https://stripe-photography-subscription.vercel.app/`
         //return_url: `http://localhost:3000`,
       });
   
